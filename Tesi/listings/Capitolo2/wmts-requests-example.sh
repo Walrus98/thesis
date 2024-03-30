d4c77d00d7992a6b0140fd262c04975631e655bc
Walrus98 <s.calugi@geckosoft.it>
2024-03-30 19:33:16 +0000

    refractoring

--- Tesi/listings/Capitolo2/wmts-requests-example.sh
+++ Tesi/listings/Capitolo2/wmts-requests-example.sh
@@ -1,8 +1,8 @@
 # Get Capabilities of the WMTS service to retrieve metadata about available layers, tile matrix sets, and operations.
-curl -o GetCapabilitiesResponse.xml "http://example.com/wmts/service?SERVICE=WMTS&REQUEST=GetCapabilities"
+curl -o GetCapabilitiesResponse.xml "http://example.com/wmts/service?service=WMTS&request=GetCapabilities"
 
 # Get a specific map tile for a given layer, tile matrix set, and tile coordinates.
-curl -o GetTileResponse.png "http://example.com/wmts/service?SERVICE=WMTS&REQUEST=GetTile&LAYER=my_layer&STYLE=default&TILEMATRIXSET=my_tile_matrix_set&TILEMATRIX=1&TILEROW=0&TILECOL=0&FORMAT=image/png"
+curl -o GetTileResponse.png "http://example.com/wmts/service?service=WMTS&request=GetTile&layer=my_layer&tileMatrixSet=my_tile_matrix_set&tileMatrix=1&tileRow=0&tileCol=0&format=image/png"
 
 # Get Feature Info for a specific layer to obtain information about features at a particular location.
-curl -o FeatureInfoResponse.xml "http://example.com/wmts/service?SERVICE=WMTS&REQUEST=GetFeatureInfo&VERSION=1.0.0&LAYER=mylayer&STYLE=default&TILEMATRIXSET=mytilematrixset&TILEMATRIX=1&TILEROW=0&TILECOL=0&FORMAT=image/png&INFOFORMAT=text/xml&I=50&J=50"
+curl -o FeatureInfoResponse.xml "http://example.com/wmts/service?SERVICE=WMTS&request=GetFeatureInfo&layer=mylayer&tileMatrixSet=mytilematrixset&tileMatrix=1&tileRow=0&tileCol=0&format=image/png&infoFormat=text/xml&I=50&J=50"
